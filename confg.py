0d70428d1016b31a1e6e7d565523b49d7fce2aa5
felixbiggy2026-design <felixbiggy2026@gmail.com>
2026-06-21 18:25:24 +0300

    Delete confg.py

--- confg.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "change-this-in-production")
-    SQLALCHEMY_DATABASE_URI = "sqlite:///users.db"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
